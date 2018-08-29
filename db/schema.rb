dda539c34cc1cc03b4c43ac5ad222188057afbc9
Ethan Waterson <elwaterson@gmail.com>
2018-08-29 11:21:39 -0400

    modified contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_08_04_170024) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "comments"
+    t.text "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
